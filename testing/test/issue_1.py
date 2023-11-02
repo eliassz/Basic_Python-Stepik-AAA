698f78f8f390ee368036549ca609d5e39de3d2f3
Ilya Lomonosov <lomonosov.iv@phystech.edu>
2023-11-03 01:52:42 +0400

    [ISSUE_1]

---
+++ testing/test/issue_1.py
@@ -0,0 +1,44 @@
+LETTER_TO_MORSE = {
+    'A': '.-', 'B': '-...', 'C': '-.-.',
+    'D': '-..', 'E': '.', 'F': '..-.',
+    'G': '--.', 'H': '....', 'I': '..',
+    'J': '.---', 'K': '-.-', 'L': '.-..',
+    'M': '--', 'N': '-.', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-',
+    'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..', '1': '.----',
+    '2': '..---', '3': '...--', '4': '....-',
+    '5': '.....', '6': '-....', '7': '--...',
+    '8': '---..', '9': '----.', '0': '-----',
+    ', ': '--..--', '.': '.-.-.-', '?': '..--..',
+    '/': '-..-.', '-': '-....-', '(': '-.--.', ')': '-.--.-',
+    ' ': ' '
+}
+
+
+def encode(message: str) -> str:
+    """
+    Кодирует строку в соответсвие с таблицей азбуки Морзе
+
+    >>> len(encode('EXTREMELY LONG INFORMATIVE MESSAGE')) # doctest: +ELLIPSIS
+    11...
+    >>> encode('FOO BAZ BAR')
+    '..-. --- ---   -... .- --..   -... .- .-.'
+    >>> encode('SOS')
+    '... --- ...'
+    >>> encode(1)
+    Traceback (most recent call last):
+    ...
+    TypeError: 'int' object is not iterable
+    """
+    encoded_signs = [
+        LETTER_TO_MORSE[letter] for letter in message
+    ]
+
+    return ' '.join(encoded_signs)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True, optionflags=doctest.FAIL_FAST)
---
+++ testing/test/readme_1.md
@@ -0,0 +1,3 @@
+Для запуска тестов для `issue_1.py` используйте один из следующих варинтов:
+- `python issue_1.py`
+- `python -m doctest -v issue_1.py`
