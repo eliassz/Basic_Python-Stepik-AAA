60c6d0356af5d3bb0ae4bd5bb94732a081da96f5
Ilya Lomonosov <lomonosov.iv@phystech.edu>
2023-11-11 00:15:44 +0400

    [PRE-COMMIT]

--- testing/issue_2/test_issue_2.py
+++ testing/issue_2/test_issue_2.py
@@ -2,19 +2,19 @@ import pytest
 from issue_2 import decode
 
 
-@pytest.mark.parametrize("input_data, expected", [
-    (
-        '..-. --- ---',
-        'FOO',
-    ),
-    (
-        '-... .- --..',
-        'BAZ',
-    ),
-    (
-        '-... .- .-.',
-        'BAR'
-    )
-])
+@pytest.mark.parametrize(
+    "input_data, expected",
+    [
+        (
+            "..-. --- ---",
+            "FOO",
+        ),
+        (
+            "-... .- --..",
+            "BAZ",
+        ),
+        ("-... .- .-.", "BAR"),
+    ],
+)
 def test_decode(input_data, expected):
     assert decode(input_data) == expected
