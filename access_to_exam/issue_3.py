8a95e7dcabe2bcecc5bd99cf4f8dcd69dd7f8f92
Ilya Lomonosov <62444495+eliassz@users.noreply.github.com>
2023-11-20 13:59:33 +0400

    [LOAD] Template is loaded (#5)
    * [LOAD] Template is loaded
    
    * [UPDATE]
    
    * [ISSUE_2]
    
    * [ISSUE_3]
    
    * [ISSUE_4]
    
    * [FLAKE8] Length of line is extended
    
    * [ISSUE_1]
    
    * [ISSUE_5]
    
    * [DELETE] Tmp file is deleted

---
+++ access_to_exam/issue_3.py
@@ -0,0 +1,44 @@
+import math
+from typing import List
+
+
+def idf_transform(count_matrix: List[List[int]]) -> List[float]:
+    """
+    Calculate the inverse document frequency (IDF) for each term.
+
+    IDF is computed as the logarithm of the number of documents divided
+    by the number of documents that contain each term, adjusted
+    and rounded to one decimal place.
+
+    :param count_matrix: A matrix where each row represents a document
+    and each column a term frequency.
+    :return: A list of IDF values for each term.
+    """
+    n_docs = len(count_matrix)
+    docs_with_words = [0] * len(count_matrix[0])
+    idf_transformation = []
+
+    for doc in count_matrix:
+        for index, count in enumerate(doc):
+            if doc[index] != 0:
+                docs_with_words[index] += 1
+
+    for el in docs_with_words:
+        res = round(math.log((n_docs + 1) / (el + 1)) + 1, 1)
+        idf_transformation.append(res)
+
+    return idf_transformation
+
+
+if __name__ == "__main__":
+    count_matrix = [
+        [1, 1, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+    ]
+    idf_matrix = idf_transform(count_matrix)
+
+    check_idf_matrix = [1.4, 1.4, 1.0, 1.4, 1.4, 1.4, 1.4, 1.4, 1.4, 1.4, 1.4, 1.4]
+
+    assert idf_matrix == check_idf_matrix
+
+    print(idf_matrix)
